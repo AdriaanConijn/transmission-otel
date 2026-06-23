71d61e63189c0bc957dca2d93e60a24d3ea0bf10
chef <>
2026-06-23 20:24:19 +0200

    use counter for cumulative stats

--- otel/metrics/metrics.go
+++ otel/metrics/metrics.go
@@ -1,10 +1,10 @@
 package otelmetrics
 
-var PortOpen = NewGauge("port_open", "Whether the Transmission peer port is open (1) or closed (0)")
-var TorrentCount = NewGauge("torrent_count", "Total number of torrents")
-var FreeSpace = NewLabeledGauge("free_space_bytes", "Free disk space in bytes", "path")
-var DownloadSpeed = NewGauge("download_speed_bytes", "Current download speed in bytes/sec")
-var UploadSpeed = NewGauge("upload_speed_bytes", "Current upload speed in bytes/sec")
-var TorrentStatus = NewLabeledGauge("torrent_status_count", "Number of torrents per status", "status")
-var TotalDownload = NewGauge("total_download_bytes", "Total downloaded bytes")
-var TotalUpload = NewGauge("total_upload_bytes", "Total uploaded bytes")
+var PortOpen = NewGauge("transmission_port_open", "Whether the Transmission peer port is open (1) or closed (0)")
+var TorrentCount = NewGauge("transmission_torrent_count", "Total number of torrents")
+var FreeSpace = NewLabeledGauge("transmission_free_space_bytes", "Free disk space in bytes", "path")
+var DownloadSpeed = NewGauge("transmission_download_speed_bytes", "Current download speed in bytes/sec")
+var UploadSpeed = NewGauge("transmission_upload_speed_bytes", "Current upload speed in bytes/sec")
+var TorrentStatus = NewLabeledGauge("transmission_torrent_status_count", "Number of torrents per status", "status")
+var TotalDownload = NewCounter("transmission_download_bytes_total", "Total downloaded bytes")
+var TotalUpload = NewCounter("transmission_upload_bytes_total", "Total uploaded bytes")
